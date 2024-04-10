5039c75ae22b6dd875d98c1b6ba1e0508dff8fa3
Beau Vincent <96351075+BeauVinc3nt@users.noreply.github.com>
2024-04-10 16:08:34 +0100

    Add files via upload

---
+++ Object Oriented Programming/Quiz app/data.py
@@ -0,0 +1,34 @@
+# Creating questions for user to answer via dictionaries. 
+question_data = [       
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
+    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+]
+
+{"response_code":0,"results":
+ [{"type":"boolean","difficulty":"medium","category":"General Knowledge",
+   "question":"The bikini is named after the &quot;Bikini Atoll&quot;, an island where the United States conducted tests on atomic bombs.","correct_answer":"True",
+   "incorrect_answers":["False"]},
+   {"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"The word &quot;news&quot; originates from the first letters of the 4 main directions on a compass (North, East, West, South).","correct_answer":"False","incorrect_answers":["True"]},
+   {"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"Albert Einstein had trouble with mathematics when he was in school.","correct_answer":"False","incorrect_answers":["True"]},
+   {"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"Fast food restaurant chains Carl&#039;s Jr. and Hardee&#039;s are owned by the same company.","correct_answer":"True","incorrect_answers":["False"]},
+   {"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"&quot;Typewriter&quot; is the longest word that can be typed using only the first row on a QWERTY keyboard.","correct_answer":"True","incorrect_answers":["False"]},
+   {"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"An eggplant is a vegetable.","correct_answer":"False","incorrect_answers":["True"]},
+   {"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"Coca-Cola&#039;s original colour was green.","correct_answer":"False","incorrect_answers":["True"]},
+   {"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"The sum of all the numbers on a roulette wheel is 666.","correct_answer":"True","incorrect_answers":["False"]},
+   {"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"Haggis is traditionally ate on Burns Night.","correct_answer":"True","incorrect_answers":["False"]},
+   {"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"The French word to travel is &quot;Travail&quot;","correct_answer":"False","incorrect_answers":["True"]}]
+   }
+
+
+# Question data has 12 entries so length of question data = 12.
+
