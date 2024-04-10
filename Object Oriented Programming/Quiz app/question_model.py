b652356993ddb9a302174bf3c04aff016fc55528
Beau Vincent <96351075+BeauVinc3nt@users.noreply.github.com>
2024-04-10 14:27:45 +0100

    Add files via upload

---
+++ Object Oriented Programming/Quiz app/question_model.py
@@ -0,0 +1,8 @@
+# CREATING QUESTION CLASS:
+class Question:
+
+    def __init__(self, question, answer):       # Initializes object instance from the Question class
+        self.text = question        # Setting question and answer format for a singular question
+        self.answer = answer
+
+
